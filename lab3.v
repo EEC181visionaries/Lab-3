05a7073df7eae03aff8888933b1d0a4cf604443e
ChanPhilip77 <Zahl506@gmail.com>
2015-01-25 23:35:33 -0800

    Update lab3.v
    Added sdram_we_n

--- lab3.v
+++ lab3.v
@@ -1,68 +1,38 @@
-module lab3test(
-CLOCK_50,
-CLOCK2_50,
+module lab3( CLOCK_50, CLOCK2_50, 
 // SDRAM
-DRAM_ADDR,
-DRAM_BA,
-DRAM_CAS_N,
-DRAM_CKE,
-DRAM_CLK,
-DRAM_CS_N,
-DRAM_DQ,
-DRAM_LDQM,
-DRAM_RAS_N,
-DRAM_UDQM,
-DRAM_WE_N,
+DRAM_ADDR, DRAM_BA, DRAM_CAS_N, DRAM_CKE, DRAM_CLK, DRAM_CS_N, DRAM_DQ, DRAM_LDQM, DRAM_RAS_N, DRAM_UDQM, DRAM_WE_N,
 // VGA
-VGA_B,
-VGA_BLANK_N,
-VGA_CLK,
-VGA_G,
-VGA_HS,
-VGA_R,
-VGA_SYNC_N,
-VGA_VS,
+VGA_B, VGA_BLANK_N, VGA_CLK, VGA_G, VGA_HS, VGA_R, VGA_SYNC_N, VGA_VS,
 // DDR3 SDRAM
-HPS_DDR3_ADDR,
-HPS_DDR3_BA,
-HPS_DDR3_CAS_N,
-HPS_DDR3_CKE,
-HPS_DDR3_CK_N,
-HPS_DDR3_CK_P,
-HPS_DDR3_CS_N,
-HPS_DDR3_DM,
-HPS_DDR3_DQ,
-HPS_DDR3_DQS_N,
-HPS_DDR3_DQS_P,
-HPS_DDR3_ODT,
-HPS_DDR3_RAS_N,
-HPS_DDR3_RESET_N,
-HPS_DDR3_RZQ,
-HPS_DDR3_WE_N,
+HPS_DDR3_ADDR, HPS_DDR3_BA, HPS_DDR3_CAS_N, HPS_DDR3_CKE, HPS_DDR3_CK_N, HPS_DDR3_CK_P, HPS_DDR3_CS_N, HPS_DDR3_DM,
+HPS_DDR3_DQ, HPS_DDR3_DQS_N, HPS_DDR3_DQS_P, HPS_DDR3_ODT, HPS_DDR3_RAS_N, HPS_DDR3_RESET_N, HPS_DDR3_RZQ, HPS_DDR3_WE_N,
 );
+
 input CLOCK_50;
 input CLOCK2_50;
 // SDRAM
-output [12: 0] DRAM_ADDR;
-output [ 1: 0] DRAM_BA;
+output [12:0] DRAM_ADDR;
+output [1:0] DRAM_BA;
 output DRAM_CAS_N;
 output DRAM_CKE;
 output DRAM_CLK;
 output DRAM_CS_N;
-inout DRAM_DQ;
+inout [15:0] DRAM_DQ;
 output DRAM_LDQM;
 output DRAM_RAS_N;
 output DRAM_UDQM;
 output DRAM_WE_N;
+
 // VGA
-output [ 7: 0] VGA_B;
+output [7:0] VGA_B;
 output VGA_BLANK_N;
 output VGA_CLK;
-output [ 7: 0] VGA_G;
+output [7:0] VGA_G;
 output VGA_HS;
-output [ 7: 0] VGA_R;
+output [7:0] VGA_R;
 output VGA_SYNC_N;
 output VGA_VS;
+
 // DDR3 SDRAM
 output [14:0] HPS_DDR3_ADDR;
 output [2:0] HPS_DDR3_BA;
@@ -80,7 +50,9 @@ output HPS_DDR3_RAS_N;
 output HPS_DDR3_RESET_N;
 input HPS_DDR3_RZQ;
 output HPS_DDR3_WE_N;
-lab3system u0 (
+
+
+mysystem u0 (
 .system_ref_clk_clk (CLOCK_50),
 .system_ref_reset_reset (1'b0),
 // VGA Subsystem
@@ -94,6 +66,7 @@ lab3system u0 (
 .vga_R (VGA_R),
 .vga_G (VGA_G),
 .vga_B (VGA_B),
+
 // DDR3 SDRAM
 .memory_mem_a (HPS_DDR3_ADDR),
 .memory_mem_ba (HPS_DDR3_BA),
@@ -121,5 +94,11 @@ lab3system u0 (
 .sdram_dq (DRAM_DQ),
 .sdram_dqm ({DRAM_UDQM,DRAM_LDQM}),
 .sdram_ras_n (DRAM_RAS_N),
+.sdram_we_n            (DRAM_WE_N),              //                  .we_n
+
 );
+
+
+
 endmodule
+
