5477eec4d8e6900fa5b9eb4083a01df6b5885123
MaJessica9 <jamma@ucdavis.edu>
2015-01-27 17:00:50 -0800

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,27 +1,33 @@
+// Hint: Don't name the execution file time.
+
 #include <time.h>
+#include <stdio.h>
 
 int main(void)
 {
   //
   // Initialization
   //
+  printf("Initialization\n");
   clock_t start, diff;
   int msec, i;
  
   // 
   // Start timer
   //
+  printf("Start Timer\n");
   start = clock();
-  for (i = 0; i < 9999999999; i++)
+  for (i = 0; i < 99999999; i++)
   {
   }// for
   
   //
   // Win
   //
+  printf("About to WIN\n");
   diff = clock() - start;
   msec = diff * 1000 / CLOCKS_PER_SEC;
-  printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
+  printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
   
   return 0;
 }
